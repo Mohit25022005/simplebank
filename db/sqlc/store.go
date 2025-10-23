c135072a5897a45636c80b0f8e3c90e222ccf04e
Mohit <mohitswarnkar13@gmail.com>
2025-10-23 18:57:17 +0530

    Handling deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -91,7 +91,24 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams)(Transf
 			return err
 		}
 
-		//TODO:Update accounts balance
+
+
+		result.FromAccount,err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:			arg.FromAccountID,
+			Amount:     -arg.Amount,
+		})
+		if err != nil{
+			return err
+		}
+
+		result.ToAccount,err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:			arg.ToAccountID,
+			Amount: 	arg.Amount,
+		})
+		if err != nil{
+			return err
+		}
+
 
 		return nil
 	})
